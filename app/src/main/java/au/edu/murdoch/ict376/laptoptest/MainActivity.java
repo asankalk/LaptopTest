37311cf0bb606b43a9907033946f42a5b6f255b4
Asanka Munasinghe <asankalk@gmail.com>
2016-09-05 11:03:17 +0800

    Initial commit

---
+++ app/src/main/java/au/edu/murdoch/ict376/laptoptest/MainActivity.java
@@ -0,0 +1,19 @@
+package au.edu.murdoch.ict376.laptoptest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void  Test()
+    {
+        int i;
+        i =1+1;
+    }
+}
